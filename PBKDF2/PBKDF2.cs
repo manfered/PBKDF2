287f795deb241978cba71cd619ac7db650bb8c78
manfered <35757736+manfered@users.noreply.github.com>
2018-08-04 15:21:46 -0600

    Add project files.

---
+++ PBKDF2/PBKDF2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PBKDF2
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using(var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfIteration)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfIteration))
+            {
+                return rfc2898.GetBytes(32);
+            }
+        }
+    }
+}
